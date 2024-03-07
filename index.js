4a021ff88ecc7624b5d42a60e99c727bc28d3ddd
Vicky <vickyfulare1998@gmail.com>
2024-03-07 17:18:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('./app')
+    //console.log("This is my first Node Js Program");
+    // var x = 10;
+    // let y = 20;
+    // const z = 30;
+    //const arr = [2, 4, 7, 1, 3, 8, 3]
+    //     //console.log(x + y + z)
+    // if (x == 10) {
+    //     console.log("Matched");
+    // }
+    // //console.log(arr)
+    // console.log(arr[0])
+    // for (i = 1; i <= 10; i++) {
+    //     console.log(i);
+    // }
+console.log(app)
+console.log(app.x)
+console.log(app.y)
+console.log(app.z())
